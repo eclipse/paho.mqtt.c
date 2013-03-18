9af36c804728930dedcdf347de231f09b1418b10
Ian Craggs <icraggs@uk.ibm.com>
2013-03-18 15:32:53 +0000

    Add notices for dual epl/edl licensing

--- src/SSLSocket.h
+++ src/SSLSocket.h
@@ -3,8 +3,12 @@
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * and Eclipse Distribution License v1.0 which accompany this distribution. 
+ *
+ * The Eclipse Public License is available at 
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
  *    Ian Craggs, Allan Stockdill-Mander - initial implementation 
--- src/samples/stdoutsub.c
+++ src/samples/stdoutsub.c
@@ -3,11 +3,12 @@
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompanies this distribution. 
+ * and Eclipse Distribution License v1.0 which accompany this distribution. 
  *
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * The Eclipse Public License is available at 
+ *   http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at 
- * http://www.eclipse.org/org/documents/edl-v10.php.
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
  *    Ian Craggs - initial contribution
@@ -33,10 +34,8 @@
 	--password none
  
 */
-extern "C" {
 #include "MQTTClient.h"
 #include "MQTTClientPersistence.h"
-}
 
 #include <stdio.h>
 #include <signal.h>

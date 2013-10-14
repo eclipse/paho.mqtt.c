6388cd1750c03e6fed46883b7d197737ca4aa1c2
Ian Craggs <icraggs@uk.ibm.com>
2013-10-14 17:09:27 +0100

    Test3 debugging

--- test/test3.c
+++ test/test3.c
@@ -908,7 +908,7 @@ int test3a_s(struct Options options)
 	fprintf(xml, "<testcase classname=\"test3\" name=\"test 3a_s\"");
 	global_start_time = start_clock();
 	
-	if (!(assert("good rc from create", (rc = MQTTClient_create(&c, options.mutual_auth_connection, "test3a_s", 
+	if (!(assert("good rc from create", (rc = MQTTClient_create(&c, options.server_auth_connection, "test3a_s", 
 		MQTTCLIENT_PERSISTENCE_DEFAULT, persistenceStore)) == MQTTCLIENT_SUCCESS, "rc was %d\n", rc)))
 		goto exit;
 
@@ -989,7 +989,7 @@ int test3a_m(struct Options options)
 	fprintf(xml, "<testcase classname=\"test3\" name=\"test 3a_m\"");
 	global_start_time = start_clock();
 
-	if (!(assert("good rc from create", (rc = MQTTClient_create(&c, options.mutual_auth_connection, "test3a_m", 
+	if (!(assert("good rc from create", (rc = MQTTClient_create(&c, options.server_auth_connection, "test3a_m", 
 		MQTTCLIENT_PERSISTENCE_DEFAULT, persistenceStore)) == MQTTCLIENT_SUCCESS, "rc was %d\n", rc)))
 		goto exit;
 

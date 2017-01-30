c2b1298576cb3f4e1cebb25243cf3d2bcd436053
Ian Craggs <icraggs@uk.ibm.com>
2017-01-30 16:59:21 +0000

    Add Travis install of OpenSSL

---
+++ travis-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+	sudo apt-get update -qq
+	sudo apt-get install -y libssl-dev
+fi
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+	brew update
+	brew install openssl 
+fi

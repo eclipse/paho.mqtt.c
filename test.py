0ef13e33515424abf49c7a4960ae79be7431d92b
Ian Craggs <icraggs@uk.ibm.com>
2017-03-27 23:51:28 +0100

    More

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 
-
-print "this is the test Python program"
-
-
 from __future__ import print_function
 
-# Trace MQTT traffic
-import MQTTV3112 as MQTTV3
+print("this is the test Python program")
 
 import socket, sys, select, traceback, datetime, os
 import SocketServer as socketserver
+
+# Trace MQTT traffic
+import MQTTV3112 as MQTTV3

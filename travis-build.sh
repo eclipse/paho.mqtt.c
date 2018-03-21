9c4a917fc2bc0ef27de1e073f7e881b6b0c215e0
Ian Craggs <icraggs@uk.ibm.com>
2018-03-21 10:35:39 +0000

    Allow test proxy to work with either Python 2 or 3

--- travis-build.sh
+++ travis-build.sh
@@ -8,7 +8,7 @@ cd build.paho
 echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
 cmake -DPAHO_WITH_SSL=TRUE -DPAHO_BUILD_DOCUMENTATION=FALSE -DPAHO_BUILD_SAMPLES=TRUE ..
 make
-python ../test/mqttsas2.py &
+python ../test/mqttsas.py &
 ctest -VV --timeout 600
 kill %1
 #killall mosquitto

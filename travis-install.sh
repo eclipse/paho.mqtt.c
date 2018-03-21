b587efa4d948a27cdaf11cb5ff5d07fd593f9eaf
Ian Craggs <icraggs@uk.ibm.com>
2018-03-21 10:38:32 +0000

    On MacOS upgrade Python to 3

--- travis-install.sh
+++ travis-install.sh
@@ -21,9 +21,9 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	# brew services stop mosquitto
 	# /usr/local/sbin/mosquitto -c test/tls-testing/mosquitto.conf &
 
-	brew install python3
+	brew upgrade python
 	git clone https://github.com/eclipse/paho.mqtt.testing.git
 	cd paho.mqtt.testing/interoperability
-	python3 startbroker.py -c localhost_testing.conf &
+	python startbroker.py -c localhost_testing.conf &
 	cd ../..
 fi

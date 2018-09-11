2ffc5a023861158c73fffe77b3810a5e370c788e
Ian Craggs <icraggs@uk.ibm.com>
2018-09-11 17:21:39 +0100

    Deploy debugging

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,1 +1,1 @@
-scp -o StrictHostKeyChecking=no build.paho/*.tar.gz icraggs@build.eclipse.org:../../../../shared/technology/paho/C/
+scp -v -o StrictHostKeyChecking=no build.paho/*.tar.gz icraggs@build.eclipse.org:../../../../shared/technology/paho/C/

99d896fc5659412ffa870d2aeb27223d9d08e276
Ian Craggs <icraggs@uk.ibm.com>
2019-07-30 20:40:41 +0100

    Reduce size of message in big message test for Travis MacOS test

--- test/test5.c
+++ test/test5.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2018 IBM Corp.
+ * Copyright (c) 2012, 2019 IBM Corp.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -84,7 +84,7 @@ struct Options
 	NULL,
 	0,
 	0,
-	5000000,
+	1000000,
 	0,
 };
 

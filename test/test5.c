f7abd15641da541250a4cd72c40e34710629c0d5
Ian Craggs <icraggs@uk.ibm.com>
2019-08-01 14:07:08 +0100

    MQTT protocol debug for big message test

--- test/test5.c
+++ test/test5.c
@@ -2552,9 +2552,9 @@ int main(int argc, char** argv)
 	}
 	else
 	{
-		/*if (options.test_no == 10)
-			MQTTAsync_setTraceLevel(MQTTASYNC_TRACE_MINIMUM);
-		else*/
+		if (options.test_no == 10)
+			MQTTAsync_setTraceLevel(MQTTASYNC_TRACE_PROTOCOL);
+		else
 			MQTTAsync_setTraceLevel(MQTTASYNC_TRACE_ERROR);
 		rc = tests[options.test_no](options); /* run just the selected test */
 	}

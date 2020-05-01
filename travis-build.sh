aecdf1c4764439804b9a2fb866ce54fd0e303420
Ian Craggs <icraggs@gmail.com>
2020-05-01 11:04:58 +0100

    More Linux build variants

--- travis-build.sh
+++ travis-build.sh
@@ -9,7 +9,7 @@ echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD with OpenSSL root $OPENSSL_ROO
 cmake -DPAHO_BUILD_STATIC=$PAHO_BUILD_STATIC -DPAHO_BUILD_SHARED=$PAHO_BUILD_SHARED -DCMAKE_BUILD_TYPE=Debug -DPAHO_WITH_SSL=TRUE -DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR -DPAHO_BUILD_DOCUMENTATION=FALSE -DPAHO_BUILD_SAMPLES=TRUE ..
 cmake --build .
 python3 ../test/mqttsas.py &
-#ctest -VV --timeout 600
+ctest -VV --timeout 600
 cpack --verbose
 kill %1
 #killall mosquitto

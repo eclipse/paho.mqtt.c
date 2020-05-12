f003f0c658465afea8ad6e839d637a93cc8a1297
Ian Craggs <icraggs@gmail.com>
2020-05-12 12:10:48 +0100

    Check outbound message list when assigning message id #649

--- src/MQTTAsync.c
+++ src/MQTTAsync.c
@@ -3472,6 +3472,7 @@ static int MQTTAsync_assignMsgId(MQTTAsyncs* m)
 
 	msgid = (msgid == MAX_MSG_ID) ? 1 : msgid + 1;
 	while (ListFindItem(commands, &msgid, cmdMessageIDCompare) ||
+			ListFindItem(m->c->outboundMsgs, &msgid, messageIDCompare) ||
 			ListFindItem(m->responses, &msgid, cmdMessageIDCompare))
 	{
 		msgid = (msgid == MAX_MSG_ID) ? 1 : msgid + 1;

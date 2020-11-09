b0c490cb791d7707a19da4350490241009232d0e
Christopher Taylor <cstaylor@argo.ai>
2020-11-09 11:16:04 -0800

    Made changes requested by upstream project

--- test/pkcs11_hsm_test/libp11_readhsm.c
+++ test/pkcs11_hsm_test/libp11_readhsm.c
@@ -1,35 +1,3 @@
-/*
- * Copyright © 2020, Andreas Jellinghaus <andreas@ionisiert.de>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 /* libp11 example code: readhsm.c
  *
  * This examply simply connects to the softhsm
@@ -62,8 +30,6 @@ int main(int argc, char *argv[])
 
     /* This is just an example. This should be changed properly. */
     char *password_table[3] = {"yksecret1", "yksecret2", "yksecret3"};
-
-
 	unsigned char *random = NULL, *signature = NULL;
 
 	char password[20];
@@ -75,8 +41,6 @@ int main(int argc, char *argv[])
 	/* load pkcs #11 module */
 	rc = PKCS11_CTX_load(ctx, SOFTHSM_LIBRARY_PATH);
 	if (rc) {
-		fprintf(stderr, "loading pkcs11 engine failed: %s\n",
-			ERR_reason_error_string(ERR_get_error()));
 		rc = 1;
 		goto nolib;
 	}
@@ -84,31 +48,22 @@ int main(int argc, char *argv[])
 	/* get information on all slots */
 	rc = PKCS11_enumerate_slots(ctx, &slots, &nslots);
 	if (rc < 0) {
-		fprintf(stderr, "no slots available\n");
 		rc = 2;
 		goto noslots;
 	}
 
-    printf("%d number of slots are found!\n", nslots);
-
 	slot = PKCS11_find_token(ctx, slots, nslots);
     for (unsigned int i = 1; i <= nslots; i++) {
         if (slot == NULL || slot->token == NULL || slot->token->label[0] == NULL) {
-            fprintf(stderr, "no token!\n");
             goto notoken;
         }
-	    printf("#%u token: label(%s), serial(%s), login-required(0x%x)\n", \
-            i, slot->token->label, slot->token->serialnr, slot->token->loginRequired);
-
         /* get all certs */
         rc = PKCS11_enumerate_certs(slot->token, &certs, &ncerts);
         if (rc) {
-            fprintf(stderr, "PKCS11_enumerate_certs failed\n");
             rc = 13;
             goto failed;
         }
         if (ncerts <= 0) {
-            fprintf(stderr, "no certificates found\n");
             rc = 14;
             goto failed;
         }
@@ -116,19 +71,16 @@ int main(int argc, char *argv[])
             PKCS11_CERT *cert = &certs[j-1];
             unsigned short id;
             memcpy(&id, cert->id, 2);
-            printf("  #%u certificate: label(%s), id(0x%x), x509(%p)\n", \
             j, cert->label, ntohs(id), cert->x509);
         }
 
         /* get all public keys */
         rc = PKCS11_enumerate_public_keys(slot->token, &keys, &nkeys);
         if (rc) {
-            fprintf(stderr, "PKCS11_enumerate_public_keys failed\n");
             rc = 13;
             goto failed;
         }
         if (nkeys <= 0) {
-            fprintf(stderr, "no public key found\n");
             rc = 14;
             goto failed;
         }
@@ -138,14 +90,12 @@ int main(int argc, char *argv[])
             memcpy(&id, key->id, 2);
             EVP_PKEY *evp_key;
             evp_key = PKCS11_get_public_key(key);
-            printf("  #%u public key: label(%s), id(0x%x), login(%d), evp_key(%p)\n", \
             j, key->label, ntohs(id), key->needLogin, key->evp_key);
         }
 
         /* perform pkcs #11 login */
         rc = PKCS11_login(slot, 0, password_table[i-1]);
         if (rc) {
-            fprintf(stderr, "PKCS11_login failed\n");
             rc = 12;
             goto failed;
         }
@@ -153,12 +103,10 @@ int main(int argc, char *argv[])
         /* get all private keys */
         rc = PKCS11_enumerate_keys(slot->token, &keys, &nkeys);
         if (rc) {
-            fprintf(stderr, "PKCS11_enumerate_keys failed\n");
             rc = 13;
             goto failed;
         }
         if (nkeys <= 0) {
-            fprintf(stderr, "no private key found\n");
             rc = 14;
             goto failed;
         }
@@ -168,16 +116,10 @@ int main(int argc, char *argv[])
             memcpy(&id, key->id, 2);
             EVP_PKEY *evp_key;
             evp_key = PKCS11_get_private_key(key);
-            printf("  #%u private key: label(%s), id(0x%x), login(%d), evp_key(%p)\n", \
-            j, key->label, ntohs(id), key->needLogin, key->evp_key);
         }
 
         slot = PKCS11_find_next_token(ctx, slots, nslots, slot);
     }
-
-
-
-
 
 	/* use the first cert */
 	authcert=&certs[0];
@@ -191,24 +133,18 @@ int main(int argc, char *argv[])
 
 	fd = open(RANDOM_SOURCE, O_RDONLY);
 	if (fd < 0) {
-		fprintf(stderr, "fatal: cannot open RANDOM_SOURCE: %s\n",
-				strerror(errno));
 		rc = 16;
 		goto failed;
 	}
 
 	rc = read(fd, random, RANDOM_SIZE);
 	if (rc < 0) {
-		fprintf(stderr, "fatal: read from random source failed: %s\n",
-			strerror(errno));
 		close(fd);
 		rc = 17;
 		goto failed;
 	}
 
 	if (rc < RANDOM_SIZE) {
-		fprintf(stderr, "fatal: read returned less than %d<%d bytes\n",
-			rc, RANDOM_SIZE);
 		close(fd);
 		rc = 18;
 		goto failed;
@@ -218,7 +154,6 @@ int main(int argc, char *argv[])
 
 	authkey = PKCS11_find_key(authcert);
 	if (authkey == NULL) {
-		fprintf(stderr, "no key matching certificate available\n");
 		rc = 19;
 		goto failed;
 	}
@@ -234,7 +169,6 @@ int main(int argc, char *argv[])
 	rc = PKCS11_sign(NID_sha1, random, RANDOM_SIZE,
 		signature, &siglen, authkey);
 	if (rc != 1) {
-		fprintf(stderr, "fatal: pkcs11_sign failed\n");
 		rc = 21;
 		goto failed;
 	}
@@ -242,7 +176,6 @@ int main(int argc, char *argv[])
 	/* verify the signature */
 	pubkey = X509_get_pubkey(authcert->x509);
 	if (pubkey == NULL) {
-		fprintf(stderr, "could not extract public key\n");
 		rc = 22;
 		goto failed;
 	}
@@ -255,7 +188,6 @@ int main(int argc, char *argv[])
 			signature, siglen, pubkey->pkey.rsa);
 #endif
 	if (rc != 1) {
-		fprintf(stderr, "fatal: RSA_verify failed\n");
 		rc = 23;
 		goto failed;
 	}
@@ -280,11 +212,6 @@ noslots:
 
 nolib:
 	PKCS11_CTX_free(ctx);
-
-	if (rc)
-		printf("authentication failed.\n");
-	else
-		printf("authentication successfull.\n");
 	return rc;
 }
 

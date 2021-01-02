e1513b265ab805783c141766dd828fe7f50c3ace
Christopher Taylor <cstaylor@argo.ai>
2021-01-02 03:35:29 -0800

    Formatting cleanup again
    Signed-off-by: Christopher Taylor <cstaylor@argo.ai>

--- test/pkcs11_hsm_test/libp11_readhsm.c
+++ test/pkcs11_hsm_test/libp11_readhsm.c
@@ -19,41 +19,41 @@
 
 int main(int argc, char *argv[])
 {
-	PKCS11_CTX *ctx;
+    PKCS11_CTX *ctx;
     PKCS11_SLOT *slots, *slot;
     PKCS11_KEY *keys;
-	PKCS11_CERT *certs;
+    PKCS11_CERT *certs;
 
-	PKCS11_KEY *authkey;
-	PKCS11_CERT *authcert;
-	EVP_PKEY *pubkey = NULL;
+    PKCS11_KEY *authkey;
+    PKCS11_CERT *authcert;
+    EVP_PKEY *pubkey = NULL;
 
     /* This is just an example. This should be changed properly. */
     char *password_table[1] = {"abcdefg"};
-	unsigned char *random = NULL, *signature = NULL;
+    unsigned char *random = NULL, *signature = NULL;
 
-	char password[20];
-	int rc, fd, logged_in;
-	unsigned int nslots, nkeys, ncerts, siglen;
+    char password[20];
+    int rc, fd, logged_in;
+    unsigned int nslots, nkeys, ncerts, siglen;
 
-	ctx = PKCS11_CTX_new();
+    ctx = PKCS11_CTX_new();
 
-	/* load pkcs #11 module */
-	rc = PKCS11_CTX_load(ctx, SOFTHSM_LIBRARY_PATH);
-	if (rc) {
-		rc = 1;
-		goto nolib;
-	}
+    /* load pkcs #11 module */
+    rc = PKCS11_CTX_load(ctx, SOFTHSM_LIBRARY_PATH);
+    if (rc) {
+        rc = 1;
+        goto nolib;
+    }
 
-	/* get information on all slots */
-	rc = PKCS11_enumerate_slots(ctx, &slots, &nslots);
-	if (rc < 0) {
-		rc = 2;
-		goto noslots;
-	}
+    /* get information on all slots */
+    rc = PKCS11_enumerate_slots(ctx, &slots, &nslots);
+    if (rc < 0) {
+        rc = 2;
+        goto noslots;
+    }
 
-	slot = PKCS11_find_token(ctx, slots, nslots);
-	for (unsigned int i = 1; i <= nslots; i++) {
+    slot = PKCS11_find_token(ctx, slots, nslots);
+    for (unsigned int i = 1; i <= nslots; i++) {
         if (!slot || !slot->token || !slot->token->label ) {
             goto notoken;
         }
@@ -119,98 +119,98 @@ int main(int argc, char *argv[])
         slot = PKCS11_find_next_token(ctx, slots, nslots, slot);
     }
 
-	/* use the first cert */
-	authcert=&certs[0];
+    /* use the first cert */
+    authcert=&certs[0];
 
-	/* get random bytes */
-	random = OPENSSL_malloc(RANDOM_SIZE);
-	if (random == NULL) {
-		rc = 15;
-		goto failed;
-	}
+    /* get random bytes */
+    random = OPENSSL_malloc(RANDOM_SIZE);
+    if (random == NULL) {
+        rc = 15;
+        goto failed;
+    }
 
-	fd = open(RANDOM_SOURCE, O_RDONLY);
-	if (fd < 0) {
-		rc = 16;
-		goto failed;
-	}
+    fd = open(RANDOM_SOURCE, O_RDONLY);
+    if (fd < 0) {
+        rc = 16;
+        goto failed;
+    }
 
-	rc = read(fd, random, RANDOM_SIZE);
-	if (rc < 0) {
-		close(fd);
-		rc = 17;
-		goto failed;
-	}
+    rc = read(fd, random, RANDOM_SIZE);
+    if (rc < 0) {
+        close(fd);
+        rc = 17;
+        goto failed;
+    }
 
-	if (rc < RANDOM_SIZE) {
-		close(fd);
-		rc = 18;
-		goto failed;
-	}
+    if (rc < RANDOM_SIZE) {
+        close(fd);
+        rc = 18;
+        goto failed;
+    }
 
-	close(fd);
+    close(fd);
 
-	authkey = PKCS11_find_key(authcert);
-	if (authkey == NULL) {
-		rc = 19;
-		goto failed;
-	}
+    authkey = PKCS11_find_key(authcert);
+    if (authkey == NULL) {
+        rc = 19;
+        goto failed;
+    }
 
-	/* ask for a sha1 hash of the random data, signed by the key */
-	siglen = MAX_SIGSIZE;
-	signature = OPENSSL_malloc(MAX_SIGSIZE);
-	if (signature == NULL) {
-		rc = 20;
-		goto failed;
-	}
+    /* ask for a sha1 hash of the random data, signed by the key */
+    siglen = MAX_SIGSIZE;
+    signature = OPENSSL_malloc(MAX_SIGSIZE);
+    if (signature == NULL) {
+        rc = 20;
+        goto failed;
+    }
 
-	rc = PKCS11_sign(NID_sha1, random, RANDOM_SIZE,
-		signature, &siglen, authkey);
-	if (rc != 1) {
-		rc = 21;
-		goto failed;
-	}
+    rc = PKCS11_sign(NID_sha1, random, RANDOM_SIZE,
+        signature, &siglen, authkey);
+    if (rc != 1) {
+        rc = 21;
+        goto failed;
+    }
 
-	/* verify the signature */
-	pubkey = X509_get_pubkey(authcert->x509);
-	if (pubkey == NULL) {
-		rc = 22;
-		goto failed;
-	}
+    /* verify the signature */
+    pubkey = X509_get_pubkey(authcert->x509);
+    if (pubkey == NULL) {
+        rc = 22;
+        goto failed;
+    }
 
-	/* now verify the result */
-	rc = RSA_verify(NID_sha1, random, RANDOM_SIZE,
+    /* now verify the result */
+    rc = RSA_verify(NID_sha1, random, RANDOM_SIZE,
 #if OPENSSL_VERSION_NUMBER >= 0x10100003L && !defined(LIBRESSL_VERSION_NUMBER)
-			signature, siglen, EVP_PKEY_get0_RSA(pubkey));
+            signature, siglen, EVP_PKEY_get0_RSA(pubkey));
 #else
-			signature, siglen, pubkey->pkey.rsa);
+            signature, siglen, pubkey->pkey.rsa);
 #endif
-	if (rc != 1) {
-		rc = 23;
-		goto failed;
-	}
+    if (rc != 1) {
+        rc = 23;
+        goto failed;
+    }
 
-	rc = 0;
+    rc = 0;
 
 failed:
-	if (rc)
-		ERR_print_errors_fp(stderr);
-	if (random != NULL)
-		OPENSSL_free(random);
-	if (pubkey != NULL)
-		EVP_PKEY_free(pubkey);
-	if (signature != NULL)
-		OPENSSL_free(signature);
+    if (rc)
+        ERR_print_errors_fp(stderr);
+    if (random != NULL)
+        OPENSSL_free(random);
+    if (pubkey != NULL)
+        EVP_PKEY_free(pubkey);
+    if (signature != NULL)
+        OPENSSL_free(signature);
 
 notoken:
-	PKCS11_release_all_slots(ctx, slots, nslots);
+    PKCS11_release_all_slots(ctx, slots, nslots);
 
 noslots:
-	PKCS11_CTX_unload(ctx);
+    PKCS11_CTX_unload(ctx);
 
 nolib:
-	PKCS11_CTX_free(ctx);
-	return rc;
+    PKCS11_CTX_free(ctx);
+    return rc;
 }
 
 /* vim: set noexpandtab: */

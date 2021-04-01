674ce08bba0521178dce9b2b7096cd3a30693c9e
Ian Craggs <icraggs@gmail.com>
2021-04-01 13:51:29 +0100

    Allow for command failure on MacOS

--- travis-build.sh
+++ travis-build.sh
@@ -10,7 +10,7 @@ cmake -DPAHO_BUILD_STATIC=$PAHO_BUILD_STATIC -DPAHO_BUILD_SHARED=$PAHO_BUILD_SHA
 cmake --build .
 python3 ../test/mqttsas.py &
 ctest -VV --timeout 600
-killall python3
+killall python3 || true
 sleep 3 # allow broker time to terminate and report
 #killall mosquitto
 cpack --verbose

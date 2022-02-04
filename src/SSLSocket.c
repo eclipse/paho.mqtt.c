7baa11e3309aa506272733d2d21971a45b90d8e1
Ian Craggs <icraggs@gmail.com>
2022-02-04 21:31:19 +0000

    Add OpenSSL version conditional around set security level

--- src/SSLSocket.c
+++ src/SSLSocket.c
@@ -593,7 +593,9 @@ int SSLSocket_createContext(networkHandles* net, MQTTClient_SSLOptions* opts)
 		}
 	}
 
+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L)
 	SSL_CTX_set_security_level(net->ctx, 1);
+#endif
 
 	if (opts->keyStore)
 	{
